5c505e659e169674286bcbbab6a62f37be4fc299
Admin <Admin@214-13>
2022-01-24 23:31:24 -0800

    Add project files.

---
+++ BizinessTemplate/Data/AppDbContext.cs
@@ -0,0 +1,23 @@
+﻿using BizinessTemplate.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BizinessTemplate.Data
+{
+    public class AppDbContext:IdentityDbContext
+    {
+
+        public AppDbContext(DbContextOptions options):base(options)
+        {
+
+        }
+
+
+        public DbSet<Service> Services { get; set; }
+        public DbSet<CustomUser> CustomUsers { get; set; }
+    }
+}
---
+++ BizinessTemplate/Models/Service.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BizinessTemplate.Models
+{
+    public class Service
+    {
+        [Key]
+        public int Id { get; set; }
+        
+        public string Icon { get; set; }
+        [MaxLength(50)]
+        public string Title { get; set; }
+        [MaxLength(250)]
+        public string Content { get; set; }
+    }
+}
